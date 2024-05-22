285b21b1a4fe146d5c7af891e4a175b5fc69f891
NIKX <tiger.zubairov@gmail.com>
2024-05-22 22:37:10 +0300

    лексер осталось подключить

--- text_transformer/main.cpp
+++ text_transformer/main.cpp
@@ -2,31 +2,52 @@
 #include <fstream>
 #include <string>
 #include <nlohmann/json.hpp>
-int main(){
+
+int main(int argc, char * argv[] ){
+
+    if (argc == 0){
+        return 1;
+    }
 
     nlohmann::json objJson;
 
-    std::ifstream jsonFile("./dictionary.json");
-
-    if (!jsonFile.is_open()){
+    std::ifstream fileInput("C:/coursework/text_transformer/assets/file.txt");
+    if (!fileInput.is_open()){
         return -1;
     }
-    
+
+    std::ifstream jsonFile("../dictionary.json");
+
+    if (!jsonFile.is_open()){
+        return -2;
+    }
+
     jsonFile >> objJson;
 
     jsonFile.close();
 
-    std::cout << objJson << std::endl;
-    for (auto &x : objJson.items())
-    {
-        std::cout << x.key() << std::endl << std::endl;
+    std::string line;
 
-        for (auto &y: x.value().items()){
-            std::cout << to_string(y.value()) << std::endl;
+    while(getline(fileInput,line)){
+        auto arrayKeyValue = objJson.items();    
+        for (auto &x : arrayKeyValue)
+        {   
+            auto arrayKeySynoword = x.value().items();
+            for (auto &y: arrayKeySynoword){
+                std::string syno = y.value().get<std::string>();
+                int index = line.find(syno);
+                if (index != std::string::npos) {
+                    std::cout << "replace string " << line.substr(index, syno.size() + 1) << " to " << x.key() << std::endl;
+                    std::cout << line << std::endl << std::endl << std::endl;
+                    line.replace(index, syno.size(), x.key());
+                   
+                    std::cout << line << std::endl << std::endl << std::endl;
+
+                }
+            }
         }
-        std::cout << std::endl;
     }
 
-    
+
     return 0;
 }
